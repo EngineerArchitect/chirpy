df4f0a964618b4c6032a89a7b1370c7d43adafb1
John Eatwell <>
2025-02-02 17:48:51 +0200

    C9l4 Sorting Chipt

--- handler_chirps_get.go
+++ handler_chirps_get.go
@@ -47,6 +47,12 @@ func (cfg *apiConfig) handlerChirpsRetrieve(w http.ResponseWriter, r *http.Reque
 		}
 	}
 
+	sortDirection := "asc"
+	sortDirectionParam := r.URL.Query().Get("sort")
+	if sortDirectionParam == "desc" {
+		sortDirection = "desc"
+	}
+
 	chirps := []Chirp{}
 	for _, dbChirp := range dbChirps {
 		if authorID != uuid.Nil && dbChirp.UserID != authorID {
@@ -63,6 +69,9 @@ func (cfg *apiConfig) handlerChirpsRetrieve(w http.ResponseWriter, r *http.Reque
 	}
 
 	sort.Slice(chirps, func(i, j int) bool {
+		if sortDirection == "desc" {
+			return chirps[i].CreatedAt.After(chirps[j].CreatedAt)
+		}
 		return chirps[i].CreatedAt.Before(chirps[j].CreatedAt)
 	})
 
